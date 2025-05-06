0e8bb8ee87f8e3db3de4b5b868dec08deb9db35e
Razvan Loghin <R.Loghin@student.tudelft.nl>
2025-05-06 19:47:56 +0200

    Added the realease pipeline

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import os
+
+version_ns = {}
+with open(os.path.join("lib_ml", "version.py")) as f:
+    exec(f.read(), version_ns)
+
+setup(
+    name="lib-ml",
+    version=version_ns["__version__"],
+    packages=find_packages(),
+    install_requires=[
+        "nltk",
+        "scikit-learn",
+        "pandas"
+    ],
+    python_requires=">=3.8",
+)
